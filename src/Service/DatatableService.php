f5bc896de717be832317598c1d1ff2fc5d957d9c
tacman <info@survos-ci.com>
2023-12-14 13:51:13 +0000

    remove debug

--- src/Service/DatatableService.php
+++ src/Service/DatatableService.php
@@ -64,7 +64,6 @@ class DatatableService
 //            dd($c);
 
             $column = new Column(...$c);
-            dump($column, $c);
             $existingSettings = $settings[$columnName]??null;
             if ($existingSettings) {
                 $options = (new OptionsResolver())
@@ -72,6 +71,7 @@ class DatatableService
                         'searchable' => false,
                         'order' => 100,
                         'sortable' => false,
+                        'is_primary' => false,
                         'browsable' => false
                     ])->resolve($existingSettings);
                 $column->searchable = $options['searchable'];

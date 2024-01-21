f51c8186557f84ad3d8ecc1af6c9c1edfcfb4d4d
tacman <info@survos-ci.com>
2024-01-21 16:30:43 +0000

    use Attribute route, etc.

--- src/Controller/MeiliController.php
+++ src/Controller/MeiliController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
 use Symfony\UX\Chartjs\Model\Chart;
